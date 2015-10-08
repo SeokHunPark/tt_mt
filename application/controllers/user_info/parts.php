98e04495d0316256f6c5ae6150c391dfc4030c0e
norion <shpark@norionsoft.com>
2015-10-08 18:07:03 +0900

    test

--- application/controllers/user_info/parts.php
+++ application/controllers/user_info/parts.php
@@ -41,16 +41,16 @@ class Parts extends CI_Controller
 	{
 		$this->output->enable_profiler(TRUE);
 		
-		$target_parts['user_id'] = "";
-		$target_parts['item_code'] = "";
-		$target_parts['class'] = "";
-		$target_parts['count'] = "";
-		$data['target_parts'] = $target_parts;
+		// $target_parts['user_id'] = "";
+		// $target_parts['item_code'] = "";
+		// $target_parts['class'] = "";
+		// $target_parts['count'] = "";
+		// $data['target_parts'] = $target_parts;
 		
-		$user_id = "";
-		$data['user_id'] = $user_id;
+		// $user_id = "";
+		// $data['user_id'] = $user_id;
 		
-		$parts_list = [];
+		// $parts_list = [];
 		// if (isset($_POST['game_account_id_text']) || isset($_POST['nickname_text']))
 		// {
 			// $user_id = $this->input->post('game_account_id_text', TRUE);
@@ -64,9 +64,9 @@ class Parts extends CI_Controller
 			// $_parts_list = $this->user_items_m->get_list($user_id);
 			// $parts_list = $this->make_load_data($_parts_list);
 		// }
-		$data['parts_list'] = $parts_list;
+		// $data['parts_list'] = $parts_list;
 		
-		$this->load->view('/user_info/parts_v', $data);
+		// $this->load->view('/user_info/parts_v', $data);
 	}
 	
 	// public function make_load_data($_parts_list)
