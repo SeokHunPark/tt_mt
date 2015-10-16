81674c82dfcc6ddca0f2565807248fc2406f2f96
norion <shpark@norionsoft.com>
2015-10-16 21:50:21 +0900

    test

--- application/controllers/game_management/popup_market.php
+++ application/controllers/game_management/popup_market.php
@@ -108,7 +108,7 @@ class Popup_market extends CI_Controller
 		print "redis_host : $redis_host";
 		
 		$redis = new Predis\Client('tcp://' . $redis_host);
-		// $redis->publish($channel, $message);
+		$redis->publish($channel, $message);
 		
 		$this->load_current_promotion();
 	}
