06a03f91ad40771cea35c1a2529e077501241b8e
norion <shpark@norionsoft.com>
2015-10-19 23:38:45 +0900

    admin log

--- application/controllers/admin/log_cstool.php
+++ application/controllers/admin/log_cstool.php
@@ -68,7 +68,7 @@ class Log_cstool extends CI_Controller
 				$end_date = $end_year . "-" . $end_month . "-" . $end_day . " 23:59:59";
 
 				$_log_list = $this->log_cstool_m->get_list_with_date($begin_date, $end_date);
-				print_r($_log_list);
+				
 				$begin_date = $begin_year . "-" . $begin_month . "-" . $begin_day;
 				$end_date = $end_year . "-" . $end_month . "-" . $end_day;
 				
@@ -80,18 +80,119 @@ class Log_cstool extends CI_Controller
 				
 				$offset = $this->uri->segment(7, 0);
 				#$log_list = $this->make_view_data(array_splice($_log_list, (int)$offset, (int)$size));
-				$log_list = $_log_list;
-				print_r($log_list);
+				$log_list = array_splice($_log_list, (int)$offset, (int)$size);
 			}
 			else
 			{
 				alert("날짜를 입력하십시오.");
 			}
 		}
+		else if (isset($_POST['user_search']))
+		{
+			if ($_POST['admin_name_text'] != "")
+			{
+				$admin_id = $this->input->post('admin_name_text', TRUE);
+				$_log_list = $this->log_cstool_m->get_list_with_admin_name($admin_id);
+				
+				$config['base_url'] = '/admin/log_cstool/load_log/admin_search/' . $admin_id . '/';
+				$config['total_rows'] = count($_log_list);
+				$config['per_page'] = $size;
+				$config['uri_segment'] = 6;
+				$this->pagination->initialize($config);
+				
+				$offset = $this->uri->segment(6, 0);
+				$log_list = array_splice($_log_list, (int)$offset, (int)$size);
+			}
+			else
+			{
+				$user_id = "";
+				if ($_POST['game_account_id_text'] != "")
+				{
+					$user_id = $this->input->post('game_account_id_text', TRUE);
+				}
+				else if ($_POST['nickname_text'] != "")
+				{
+					$nickname = $this->input->post('nickname_text', TRUE);
+					$user_id = $this->user_info_m->get_user_id_with_nickname($nickname);
+				}
+				
+				$_log_list = $this->log_cstool_m->get_list_with_user_id($user_id);
+				
+				$config['base_url'] = '/admin/log_cstool/load_log/user_search/' . $user_id . '/';
+				$config['total_rows'] = count($_log_list);
+				$config['per_page'] = $size;
+				$config['uri_segment'] = 6;
+				$this->pagination->initialize($config);
+				
+				$offset = $this->uri->segment(6, 0);
+				#$log_list = $this->make_view_data(array_splice($_log_list, (int)$offset, (int)$size));
+				$log_list = array_splice($_log_list, (int)$offset, (int)$size);
+			}
+		}
+		else
+		{
+			if (strcmp($mode, "date_search") == 0)
+			{
+				$offset = $this->uri->segment(7, 0);
+				$begin_date = $this->uri->segment(5, 0) . " 00:00:00";
+				$end_date = $this->uri->segment(6, 0) . " 23:59:59";
+				
+				$_log_list = $this->log_cstool_m->get_list_with_date($begin_date, $end_date);
+				$config['base_url'] = '/admin/log_cstool/load_log/date_search/' . $this->uri->segment(5, 0) . '/' . $this->uri->segment(6, 0) . '/';
+				$config['total_rows'] = count($_log_list);
+				$config['per_page'] = $size;
+				$config['uri_segment'] = 7;
+				$this->pagination->initialize($config);
+				
+				$log_list = array_splice($_log_list, (int)$offset, (int)$size);
+			}
+			else if (strcmp($mode, "user_search") == 0)
+			{
+				$offset = $this->uri->segment(6, 0);
+				$user_id = $this->uri->segment(5, 0);
+				
+				$_log_list = $this->log_cstool_m->get_list_with_user_id($user_id);
+				
+				$config['base_url'] = '/admin/log_cstool/load_log/user_search/' . $user_id . '/';
+				$config['total_rows'] = count($_log_list);
+				$config['per_page'] = $size;
+				$config['uri_segment'] = 6;
+				$this->pagination->initialize($config);
+				
+				$offset = $this->uri->segment(6, 0);
+				$log_list = array_splice($_log_list, (int)$offset, (int)$size);
+			}
+			else if (strcmp($mode, "admin_search") == 0)
+			{
+				$offset = $this->uri->segment(6, 0);
+				$admin_id = $this->uri->segment(5, 0);
+				
+				$_log_list = $this->log_cstool_m->get_list_with_admin_name($admin_id);
+				
+				$config['base_url'] = '/admin/log_cstool/load_log/user_search/' . $admin_id . '/';
+				$config['total_rows'] = count($_log_list);
+				$config['per_page'] = $size;
+				$config['uri_segment'] = 6;
+				$this->pagination->initialize($config);
+				
+				$offset = $this->uri->segment(6, 0);
+				$log_list = array_splice($_log_list, (int)$offset, (int)$size);
+			}
+		}
 		
 		$data['log_list'] = $log_list;
-		#$data['log_list'] = array();
 		$data['pagination'] = $this->pagination->create_links();
 		$this->load->view('/admin/log_cstool_v', $data);
+	}
+	
+	public function make_view_data()
+	{
+		$order_list = array();
+		for ($i = 0; $i < count($_order_list); $i++)
+		{
+			$order = (array)$_order_list[$i];
+			$order_list[$i] = $order;
+		}
+		return $order_list;
 	}
 }
--- application/models/log_cstool_m.php
+++ application/models/log_cstool_m.php
@@ -32,6 +32,26 @@ class Log_cstool_m extends CI_Model
 		return $query->result();
 	}
 	
+	function get_list_with_user_id($user_id)
+	{
+		$this->db->select('*');
+		$this->db->from('drag_logdb.log_cstool');
+		$this->db->where('user_id', $user_id);
+		$this->db->order_by("log_cstool_idx", "desc");
+		$query = $this->db->get();
+		return $query->result();
+	}
+	
+	function get_list_with_admin_name($admin_name)
+	{
+		$this->db->select('*');
+		$this->db->from('drag_logdb.log_cstool');
+		$this->db->where('admin_name', $admin_name);
+		$this->db->order_by("log_cstool_idx", "desc");
+		$query = $this->db->get();
+		return $query->result();
+	}
+	
 	function insert_log($reg_date, $ip_address, $admin_name, $user_id, $action, $item_id, $item_count, $memo)
 	{
 		$this->db->set('reg_date', $reg_date);
@@ -44,4 +64,6 @@ class Log_cstool_m extends CI_Model
 		$this->db->set('memo', $memo);
 		return $this->db->insert('drag_logdb.log_cstool');
 	}
+	
+	
 }
